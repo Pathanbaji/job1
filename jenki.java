95212f73f3d71bb4322e3d5bd45ac32cb4c46add
baji <pathanbaji123123@gmail.com>
2023-01-13 18:16:46 +0530

    first commit

---
+++ jenki.java
@@ -0,0 +1,30 @@
+package Assignment1;
+
+public class jenki {
+	public static void main(String[] args) {
+	jenki p1 = new jenki();
+	 p1.program1();
+
+	 
+
+	}
+
+	 
+
+	public void program1()
+
+	 
+
+	{
+
+	 
+
+	System.out.println("simple Java application");
+
+	}
+
+	 
+
+	}
+
+
